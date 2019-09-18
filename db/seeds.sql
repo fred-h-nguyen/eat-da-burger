033e0702f8694db3cdc207023d60668efef1b643
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-09-18 13:52:20 -0500

    db for app

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUE ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUE ('Double Cheese Burger');
+INSERT INTO burgers (burger_name) VALUE ('Goodnight Burger');
